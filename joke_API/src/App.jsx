e3bbaf21329dee0be09fb805af10e609d20257a8
katrine-mo-jensen <katrine.mo.jensen@gmail.com>
2023-08-29 12:46:19 +0200

    With correct buton now

--- joke_API/src/App.jsx
+++ joke_API/src/App.jsx
@@ -3,24 +3,27 @@ import "./App.css";
 
 function App() {
   const [joke, setJoke] = useState([]);
-  
-  useEffect(() => {
+
+  function getJoke() {
     const url = "https://official-joke-api.appspot.com/random_joke";
     fetch(url)
       .then((res) => res.json())
       .then((data) => setJoke(data));
+  }
+
+  useEffect(() => {
+    getJoke;
   }, []);
   console.log("Joke", joke);
 
   return (
     <>
       <h1>Jokes</h1>
-      <form>
-            <h3>{joke.setup} </h3>
-            <p>{joke.punchline}</p>
-            <button>New joke?</button>
-          </form>
-    
+      <div>
+        <h3>{joke.setup} </h3>
+        <p>{joke.punchline}</p>
+        <button onClick={getJoke}>New joke?</button>
+      </div>
     </>
   );
 }
